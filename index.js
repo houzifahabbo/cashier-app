a64065d8cf1f9dc78f9f3491dad70a6963d969d9
houzifahabbo <h-hebbo@hotmail.com>
2022-09-05 06:27:14 +0300

    98%

--- index.js
+++ index.js
@@ -1,67 +1,44 @@
-//TODO clean the code and take a look at the whole code
 let serial;
 var totalBeforeDiscount=0;
 var discountValue=0;
 var totalAfterDiscount=0;
-var x=0;
+const test2=[[]];
+const test5=[]
+const test3=[];
+const test4=[];
+var i=1;
+var z=0;
+var loc;
+var ex =0;
+var sum=0;
+var quantityValue=0;
+var quantitySet=0;
+var total2=0;
+var discountValueTotal=0;
+//TODO clean the code and take a look at the whole code
+//TODO rename lists
 function serialInput(evt) {
     if(event.key === 'Enter') {
         serial = document.getElementById("serialInput").value;
         array();
     }
 }
-/*function onlyNumberKey(evt) {
-    var ASCIICode = (evt.which) ? evt.which : evt.keyCode
-    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57) && (ASCIICode < 96 || ASCIICode > 106)){
-        return false;
-    }
-    else if(ASCIICode == 8){
-        if(x<=0){
-            x==1;
-        }
-        else{
-            x=x-1;
-        }
-        return true;
-    }
-    else if(ASCIICode == 13){
-        if (x==5){
-            return true;
-        }
-        else{
-            console.log("must be 5 digits");
-            return false;
-        }
-    }
-    if(x==5){
-        return false;
-    }
-    else{
-        ++x;
-        return true;
-    }
-}*/
-const test2=[[]];
-const test3=[];
-const test4=[];
-var z=0;
-var loc;
-var sum=0;
-var quantityValue=0;
-var quantitySet=0;
-//TODO rename lists
 function array(){
     const products = ["13579,apple,2,0.5", "24695,orange,5,1","73584,lemon,10,2.5"];
+    ex=0;
     quantitySet++;
+    if(z>0){
+        exist();
+    }
     for(let x in products){
         const arr= products[x].split(",");
-       if(z>0&&test2[z-1][0] == serial ){
+        if(z>0 && test2[z-1][0] == serial ){
             loc= (parseInt(test2[z-1][4]));
             test2[z-1][5]=quantitySet;
             quantityValue=test2[z-1][5]
             addMore(loc);
         }
-        else if(arr[0] == serial){
+        else if(arr[0] == serial && ex==0){
             productNameValue= arr[1];
             priceValue=arr[2];
             discountValue=arr[3];
@@ -70,17 +47,30 @@ function array(){
             quantitySet=1;
             test2[z][5]=quantitySet;
             quantityValue=test2[z][5]
+            test5[z]=serial;
             addingProducts(); 
             z++;
         }
-        else{
-            alert("wrong serial number");
-        }
     }
-    sett()
+    sett();
     edit();
 }
-var i=1;
+
+function exist(){
+    for(let x in test5){
+        if(test5[x] == serial &&z>0){
+            loc= (parseInt(test2[x][4]))
+            quantitySet=test2[x][5]
+            quantitySet++
+            test2[x][5]=quantitySet;
+            quantityValue=test2[x][5]
+            priceValue=test2[x][2];
+            discountValue=test2[x][3];
+            addMore(loc);
+            ex=1;
+        }
+    }
+}
 function addingProducts(){
     //TODO make function for const and ids
     const total=document.createElement("total"),discount=document.createElement("discount"),quantity=document.createElement("quantity"),price= document.createElement("price"),deleteBtn= document.createElement("deleteBtn"),productName=document.createElement("productName"),productsList = document.getElementById("productList"),product = document.createElement("product"),counter=document.createElement("counter");
@@ -89,6 +79,7 @@ function addingProducts(){
     quantity.setAttribute("id","quantity"+i);
     total.setAttribute("id","total"+i);
     discount.setAttribute("id","discount"+i);
+    deleteBtn.setAttribute("onClick", "removeProduct("+i+")");
     if (i%2==0){
         product.style.background= '#f6f4fe' ;
         product.classList.add("product"+i);
@@ -102,7 +93,6 @@ function addingProducts(){
     quantity.textContent=quantityValue;
     total.textContent=totalBeforeDiscount;
     discount.textContent="- "+discountValue;
-    deleteBtn.setAttribute("onClick", "removeProduct("+i+")");
     deleteBtn.textContent= "remove";
     product.append(counter , productName , deleteBtn , price ,quantity ,total,discount);
     productsList.appendChild(product);
@@ -122,7 +112,7 @@ function removeProduct(selector){
 function addMore(loc){
     getAndEditElements(("quantity"+loc),quantityValue);
     getAndEditElements(("total"+loc),totalBeforeDiscount);
-    getAndEditElements(("discount"+loc),discount);
+    getAndEditElements(("discount"+loc),"- "+discount);
 }
 
 function edit(){
@@ -134,8 +124,7 @@ function edit(){
 function getAndEditElements(id,value){
     totalBeforeDiscount=(priceValue*quantityValue)
     discount = discountValue*quantityValue;
-    discountText= "- "+discountValueTotal;
-    //totalAfterDiscount +=((priceValue-discountValue)*quantityValue);
+    discountText=discountValueTotal;
     document.getElementById(id).innerHTML= value ;
 }
 
@@ -145,30 +134,31 @@ function invoice(){
         $('#invoiceLayout').printThis({debug: false,importCSS: true,loadCSS: currentPath});
         $("deleteBtn").hide();
     });
-    setTimeout(function(){$("deleteBtn").show()},1000)
+    setTimeout(function(){$("deleteBtn").show()},1000);
 }
-var total2=0;
-var discountValueTotal=0;
+
 function sett(){
-    test3[z-1]=totalBeforeDiscount;
-    test4[z-1]=discount;
-    discountValueTotal=0
-    total2=0
+    if(ex==1){
+        test3.splice( (loc - 1) , 1 , totalBeforeDiscount );
+        test4.splice( (loc - 1) , 1 , discount );
+   }
+    else{
+        test3[z-1]=totalBeforeDiscount;
+        test4[z-1]=discount;
+    }
     const total3 = test3.reduce((accumulator, value) => {
         return accumulator + value;
     }, 0);
-    total2=total3
-      /*for (const value of test3) {
-        total2 += value;
-      }*/
-    for (const value of test4) {
-        discountValueTotal+= value;
-    }
-    totalAfterDiscount=total2-discountValueTotal
+    total2=total3;
+    const discountValueTotal2 = test4.reduce((accumulator, value) => {
+        return accumulator - value;
+    }, 0);
+    discountValueTotal=discountValueTotal2;
+    totalAfterDiscount = total2 + discountValueTotal;
 }
 function decrease(selector){
-    total2-=test3[selector-1];
-    discountValueTotal-=test4[selector-1]
-    totalAfterDiscount=total2-discountValueTotal
+    total2 -= test3[selector-1];
+    discountValueTotal += test4[selector-1];
+    totalAfterDiscount = total2 - discountValueTotal;
 
 }
